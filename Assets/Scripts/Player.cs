b45f3ef9fe99d601e8c436ed820680de73a36b8e
beepobb <yeejiazhen23@gmail.com>
2025-03-22 01:38:03 +0800

    Add collision detection with movement on walking diagonal

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using NUnit.Framework.Interfaces;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -15,8 +16,47 @@ public class Player : MonoBehaviour
 
         // Move the player
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        // Time.deltaTime to make movement framerate dependent
-        transform.position += moveDir * Time.deltaTime * moveSpeed;
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if (!canMove)
+        {
+            // Cannot move towards moveDir
+            // Attempt only x movemnt
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            
+            if (canMove)
+            {
+                // Can move only on the X axis
+                moveDir = moveDirX;
+            } else
+            {
+                // Cannot move only on the X axis
+
+                // Attempt only Z movement
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                
+                if (canMove)
+                {
+                    // Can move only on the Z
+                    moveDir = moveDirZ;
+                } else
+                {
+                    // Cannot move in any direction
+                }
+            }
+        }
+
+        if (canMove)
+        {
+            // Time.deltaTime to make movement framerate dependent
+            transform.position += moveDir * moveDistance;
+        }
 
         isWalking = moveDir != Vector3.zero;
         float rotateSpeed = 10f;
