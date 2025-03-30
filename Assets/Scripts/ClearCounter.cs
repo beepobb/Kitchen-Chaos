d4138a66af3b414aa7ecbe0716ff6fff3c1ded52
beepobb <yeejiazhen23@gmail.com>
2025-03-31 01:58:28 +0800

    Remove testing logic

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,18 +4,9 @@ public class ClearCounter : MonoBehaviour, IKitchenObjectParent {
     // Can use GameObject too
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform counterTopPoint;
-    [SerializeField] private ClearCounter secondClearCounter;
-    [SerializeField] private bool testing;
 
     private KitchenObject kitchenObject;
-
-    private void Update() {
-        if (testing && Input.GetKeyDown(KeyCode.T)) {
-            if (kitchenObject != null) {
-                kitchenObject.SetKitchenObjectParent(secondClearCounter);
-            }
-        }
-    }
+    
     public void Interact(Player player) {
         if(kitchenObject == null) {
             Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint); // Spawn the tomato
